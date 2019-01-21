6a3bc192a5739ecce4fd44c7a6532e88c2f34235
Himani Kaul <himanikaul96@gmail.com>
2019-01-21 18:52:46 +0530

    Added a share and download intent

--- app/src/main/java/com/example/himani_k/greeting_card/crop_class.java
+++ app/src/main/java/com/example/himani_k/greeting_card/crop_class.java
@@ -3,6 +3,7 @@ package com.example.himani_k.greeting_card;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import java.util.Objects;
 
@@ -11,7 +12,7 @@ public class crop_class extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
          setContentView(R.layout.crop_image_);
-         //setting back button
+             //setting back button
             Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar3);
             setSupportActionBar(toolbar);
             Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
@@ -19,9 +20,20 @@ public class crop_class extends AppCompatActivity {
             if (getSupportActionBar() != null)
              { getSupportActionBar().setDisplayHomeAsUpEnabled(true);
                getSupportActionBar().setDisplayShowHomeEnabled(true);}
-
-
-
-
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+        return super.onOptionsItemSelected(item);
+}
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/himani_k/greeting_card/image_from_gallery.java
+++ app/src/main/java/com/example/himani_k/greeting_card/image_from_gallery.java
@@ -1,8 +1,10 @@
 package com.example.himani_k.greeting_card;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -11,27 +13,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
 
+import static android.media.MediaScannerConnection.scanFile;
+
 public class image_from_gallery extends AppCompatActivity {
-    private int IMAGE_GALLERY_REQUEST =0 ;
+    private int IMAGE_GALLERY_REQUEST = 0;
     ImageView imageView;
     boolean flag;
+    Context context = null;
+    Uri imageUri = null;
+    String filename;
 
-    HorizontalScrollView horizontalScrollView;
     @Override
-    protected void onCreate( Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.image_from_gallery);
 
-        imageView=(ImageView) findViewById(R.id.imageView_plus);
-
+        imageView = (ImageView) findViewById(R.id.imageView_plus);
 
         //setting back button
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar2);
@@ -39,22 +46,23 @@ public class image_from_gallery extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
 
         // add back arrow to toolbar
-        if (getSupportActionBar() != null)
-            { getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-              getSupportActionBar().setDisplayShowHomeEnabled(true);}
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
 
         //starts the crop window
-        ImageButton crop= (ImageButton) findViewById(R.id.crop_1);
+        ImageButton crop = (ImageButton) findViewById(R.id.crop_1);
         crop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i= new Intent(image_from_gallery.this, crop_class.class);
+                Intent i = new Intent(image_from_gallery.this, crop_class.class);
                 startActivity(i);
             }
         });
 
         //open gallery
-        ImageButton gal= (ImageButton) findViewById(R.id.gallery);
+        ImageButton gal = (ImageButton) findViewById(R.id.gallery);
         gal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -62,7 +70,7 @@ public class image_from_gallery extends AppCompatActivity {
             }
         });
         //adding a delete option
-        ImageView del= (ImageView) findViewById(R.id.imageButton1);
+        ImageView del = (ImageView) findViewById(R.id.imageButton1);
         del.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -70,49 +78,97 @@ public class image_from_gallery extends AppCompatActivity {
             }
         });
 
+        //sharing image
+        ImageView share = (ImageView) findViewById(R.id.imageButton5);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareImage(imageUri);
+            }
+        });
+
+        //downloading image
+        ImageView download = (ImageView) findViewById(R.id.imageButton4);
+        download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BitmapDrawable draw = (BitmapDrawable) imageView.getDrawable();
+                Bitmap bitmap = draw.getBitmap();
+                File storage= Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES); //context.getExternalFilesDir(null);
+                File file = new File(storage,filename  + ".jpg");
+                try{
+                    FileOutputStream fos = new FileOutputStream(file);
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                    fos.close();
+                    scanFile(context, Uri.fromFile(file));
+                    } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }});
+    }
+
+         private void scanFile(Context context, Uri uri) {
+        Intent scanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        scanIntent.setData(uri);
+        context.sendBroadcast(scanIntent);
+    }
+
+    // fetching image from the gallery
+    public void on_Image_from_Gallery() {
+        //prompting gallery
+        Intent photopicker = new Intent(Intent.ACTION_PICK);
+        //Where to find the data
+        File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        String PictureDirectoryPath = pictureDirectory.getPath();
+        //getting a URI representation
+        Uri data = Uri.parse(PictureDirectoryPath);
+        photopicker.setDataAndType(data, "image/*");
+        startActivityForResult(photopicker, IMAGE_GALLERY_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == IMAGE_GALLERY_REQUEST) {
+                imageUri = data.getData();
+                InputStream inputStream;
+                try {
+                    inputStream = getContentResolver().openInputStream(imageUri);
+                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                    //setting the image in the imageView
+                    imageView.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+    }
 
-         // fetching image from the gallery
-        public void on_Image_from_Gallery() {
-            //prompting gallery
-            Intent photopicker = new Intent(Intent.ACTION_PICK);
-            //Where to find the data
-            File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-            String PictureDirectoryPath = pictureDirectory.getPath();
-            //getting a URI representation
-            Uri data = Uri.parse(PictureDirectoryPath);
-            photopicker.setDataAndType(data, "image/*");
-            startActivityForResult(photopicker, IMAGE_GALLERY_REQUEST);
-        }
-
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-            if(resultCode==RESULT_OK)
-            {
-                if(requestCode==IMAGE_GALLERY_REQUEST)
-                {
-                    Uri imageuri = data.getData();
-                    InputStream inputStream;
-                    try {
-                         inputStream=getContentResolver().openInputStream(imageuri);
-                         Bitmap bitmap=BitmapFactory.decodeStream(inputStream);
-                         //setting the image in the imageView
-                          imageView.setImageBitmap(bitmap);
-                    }
-                    catch (FileNotFoundException e) {
-                        e.printStackTrace(); } }
-            } }
-
-         @Override
-            public boolean onOptionsItemSelected(MenuItem item) {
-            // handle arrow click here
-            if (item.getItemId() == android.R.id.home) {
-                finish(); // close this activity and return to preview activity (if there is any)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
         }
 
         return super.onOptionsItemSelected(item);
     }
-        //Rotating the icon
+
+    // Share image
+    private void shareImage(Uri imagePath) {
+        Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+        sharingIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        sharingIntent.setType("image/*");
+        sharingIntent.putExtra(Intent.EXTRA_STREAM, imagePath);
+        startActivity(Intent.createChooser(sharingIntent, "Share Image Using"));
+    }
+
+
+
+}
+    //Rotating the icon
        /* public void set_gone(View view) {
         if (flag){
             // means true
@@ -128,4 +184,4 @@ public class image_from_gallery extends AppCompatActivity {
             flag=false;
             set_gone(view);
     }*/
-}
+
